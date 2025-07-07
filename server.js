8dbb28b71690d549e1e3b0b69b8c189ed8e5782f
endoxy <business.threaten413@passinbox.com>
2025-07-07 21:25:16 +0200

    Test

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 
 PORT = process.env.PORT || 3000;
+dotenv.config({ path: require('path').join(__dirname, '.env') });
 
 console.log("Server is running on Port ", {PORT});

b50c4f90a378da64cca3af11499efa1840cb3533
endoxy <business.threaten413@passinbox.com>
2025-07-08 13:16:29 +0200

    ASd

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
+//Init
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
-
-dotenv.config({ path: require('path').join(__dirname, '.env') });
-PORT = process.env.PORT || 3000;
-
 const app = express();
 
+//Middleware
+dotenv.config({ path: require('path').join(__dirname, '.env') });
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/src', express.static(path.join(__dirname, 'src')));
 
+//Env
+PORT = process.env.PORT || 3000;
+
+//Main
 app.listen(PORT);
-
 console.log(`Server is running on http://localhost:${PORT}`);
 console.log("Press Ctrl+C to stop the server.");
